b4d5f8b013046b533f842948709496d34d6ec2c5
Jussi Saurio <jussi.saurio@gmail.com>
2016-11-22 11:16:29 +0200

    process env port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.use(express.static('public')); // express.static defines a folder to expose to the webserver
 
-var port = process.env.port;
+var port = process.env.PORT;
 app.listen(port || 3000, function() {
 
 	console.log('Express server running on port 3000, perkele');
