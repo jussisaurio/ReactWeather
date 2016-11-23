209fb8976641c58596036ea8d8bff30b2547e421
Jussi Saurio <jussi.saurio@gmail.com>
2016-11-23 12:34:55 +0200

    fixed error with modal

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
 
@@ -16,20 +18,29 @@ var ErrorModal = React.createClass({
 
 	// runs immediately after component is mounted
 	componentDidMount: function() {
+		
+		var {title, message}=this.props;
+		var modalMarkup = (
+			<div id="error-modal" className="reveal tiny text-center" data-reveal="" data-close-on-esc="true">
+				<h4>{title}</h4>
+				<p>{message}</p>
+				<p><button className="expanded button" data-close="">OK</button></p>
+			</div>);
+
+		var $modal = $(ReactDOMServer.renderToString(modalMarkup)); // jQuery object
+
+		$(ReactDOM.findDOMNode(this)).html($modal); // jquery selects the DOM Node where ErrorModal lives
+		// and adds the markup to it
+
 		var modal = new Foundation.Reveal($('#error-modal')); //jquery call in parentheses
 		modal.open();
 	},
 
 	render: function() {
 
-		var {title, message}=this.props;
-
+		// these are done in componentDidMount because React doesnt work well with 3rd party libraries updating DOM
 		return(
-			<div id="error-modal" className="reveal tiny text-center" data-reveal="" data-close-on-esc="true">
-				<h4>{title}</h4>
-				<p>{message}</p>
-				<p><button className="expanded button" data-close="">OK</button></p>
-			</div>
+			<div></div>	
 		);
 	}
 });
