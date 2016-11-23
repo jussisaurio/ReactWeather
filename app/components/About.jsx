683466770dca489c731a2eb7c71fc42a2c7bbabb
Jussi Saurio <jussi.saurio@gmail.com>
2016-11-23 12:23:23 +0200

    more about text

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -15,7 +15,7 @@ var About = (props) => {
 			<div>
 			<h1 className="text-center page-title">About the app</h1>
 			<p>This app is just a means to the end of getting acquainted with React, Webpack, Foundation and some ES6 and newer experimental syntax.</p>
-			<p>The weather data is fetched from the <a href="https://openweathermap.org/api/">Open Weather Map API</a>.</p>
+			<p>The weather data is fetched from the <a href="https://openweathermap.org/api/">Open Weather Map API</a>. I had to use the Heroku CORS Anywhere server to get the HTTP requests to work from a HTTPS location.</p>
 			</div>
 		);
 };
